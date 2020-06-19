ddc01db78949858337a233075383615ef118b387
Juan Ceschin <juanceschin@gmail.com>
2020-06-18 21:09:36 -0300

    Add create feature

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,4 +6,16 @@ class FlatsController < ApplicationController
   def new
     @flat = Flat.new
   end
+
+  def create
+    @flat = Flat.find(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
